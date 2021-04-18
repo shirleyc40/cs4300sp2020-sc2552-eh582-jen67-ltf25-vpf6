8ca909f60e709b4059d45793b8af5bbeebe63a23
ngjulia <jen67@cornell.edu>
2021-04-18 16:46:01 -0400

    added additional check in while loop for bool search)

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -9,7 +9,7 @@ net_id = "Elina Hvirtsman: eh582, Julia Ng: jen67, Shirley Chen: sc2552, Luke Fo
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	url='https://bonappetit-cs.herokuapp.com/query'
+	url='http://localhost:5000/query'
 	params = {}
 	dietary = request.args.get('dietary')
 	params['ingredients'] = dietary
